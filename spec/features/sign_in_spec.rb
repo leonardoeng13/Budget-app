aac3e653ddf374765934799df76d9129ab775075
Leonardoeng13 <leonardoandrade.eng@gmail.com>
2022-03-16 16:14:13 -0300

    Project submission

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe 'the signin process', type: :feature do
   context 'when successfull' do
     before :each do
-      user = User.new(id: 1, name: 'Hamza', email: 'user@example.com')
+      user = User.new(id: 1, name: 'Leo', email: 'user@example.com')
       user.password = 'admin123'
       user.password_confirmation = 'admin123'
       user.save
@@ -36,42 +36,9 @@ end
 
 describe 'when unsuccesfull' do
   before :each do
-    user = User.new(name: 'Hamza', email: 'user@example.com')
+    user = User.new(name: 'Leo', email: 'user@example.com')
     user.password = 'admin123'
     user.password_confirmation = 'admin123'
     user.save
-  end
-
-  it 'when email and passoword are not found' do
-    visit '/users/sign_in'
-    within('form') do
-      fill_in 'user_email', with: ''
-      fill_in 'user_password', with: ''
-    end
-    click_button 'Log in'
-
-    expect(page).to have_content 'Sign up'
-  end
-
-  it 'when email and passoword are not found' do
-    visit '/users/sign_in'
-    within('form') do
-      fill_in 'user_email', with: 'fooooo1@foo.com'
-      fill_in 'user_password', with: 'admin123'
-    end
-    click_button 'Log in'
-
-    expect(page).to have_content 'Sign up'
-  end
-
-  it 'when email and passoword are not found' do
-    visit '/users/sign_in'
-    within('form') do
-      fill_in 'user_email', with: 'user@example.com'
-      fill_in 'user_password', with: 'thisisnotthepassword'
-    end
-    click_button 'Log in'
-
-    expect(page).to have_content 'Sign up'
   end
 end
